7b5e37eee2c3cf9b5b9a429e1c58791e6f8899bf
haru <haruki6180@gmail.com>
2021-03-25 16:37:20 +0900

    削除機能の権利追加

--- myapp/home/views.py
+++ myapp/home/views.py
@@ -4,14 +4,18 @@ from django.contrib.auth.models import User
 from django.shortcuts import render
 from post.forms import PostForm
 from post.models import Post, Like
+from user.helpers import get_current_user
+
 
 
 @login_required
 def home(request):
+    current_user = get_current_user(request)
     initial_dict = {'user': request.user}
     form = PostForm(request.POST or None, initial=initial_dict)
     context = {
         "user": request.user,
+        "current_user": current_user,
         "form": form,
         "posts": Post.objects.all(),
         "my_likes_ids":Like.objects.filter(
